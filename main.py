7ce06e269687352f4db241eb4da968f7041270ce
advikhmtcs <advikhm-farmington@thecoderschool.com>
2024-11-20 23:56:48 +0000

    started project

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import random
 
 wordList = ("Counterrevolutionaries", "Deinstitutionalisation" ,"oligarchy", 'proletariet', 'Supercalifragilisticexpialidocious', 'Antidisestablishmentarianism' )
 gameWord = wordList[random.randint(0,len(wordList)-1)]
-print(gameWord)
+gameLetters=[]
+gameLetters.extend(gameWord)
+underLines=[]
+for i in range(len(gameWord)):
+    underLines.append("_")
